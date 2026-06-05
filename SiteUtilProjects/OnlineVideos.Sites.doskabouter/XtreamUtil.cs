600095f78c24b18234e4cbf75dabe3930ff7a31b
doskabouter <paul.doskabouter@gmail.com>
2026-06-05 17:04:04 +0200

    Added search to Xtream

--- SiteUtilProjects/OnlineVideos.Sites.doskabouter/XtreamUtil.cs
+++ SiteUtilProjects/OnlineVideos.Sites.doskabouter/XtreamUtil.cs
@@ -2,6 +2,8 @@
 using Newtonsoft.Json.Linq;
 
 using System.ComponentModel;
+using System.Linq;
+using System;
 
 namespace OnlineVideos.Sites
 {
@@ -16,16 +18,21 @@ namespace OnlineVideos.Sites
         [Category("OnlineVideosUserConfiguration"), Description("The password for accessing the server")]
         private string password = null;
 
+        private Dictionary<string, string> categoryNames = new Dictionary<string, string>();
+
         public override int DiscoverDynamicCategories()
         {
             var data = GetWebData<JToken>(getUrl() + "get_live_categories");
             foreach (var obj in data)
             {
+                var name = obj.Value<string>("category_name").Replace('|', '∣');
+                var id = obj.Value<string>("category_id");
                 RssLink cat = new RssLink()
                 {
-                    Name = obj.Value<string>("category_name").Replace('|', '∣'),
-                    Url = getUrl() + "get_live_streams&category_id=" + obj.Value<string>("category_id")
+                    Name = name,
+                    Url = getUrl() + "get_live_streams&category_id=" + id
                 };
+                categoryNames[id] = name;
                 Settings.Categories.Add(cat);
             }
             Settings.DynamicCategoriesDiscovered = true;
@@ -41,21 +48,64 @@ namespace OnlineVideos.Sites
                 var data = GetWebData<JToken>(cat.Url);
                 foreach (var obj in data)
                 {
-                    VideoInfo vid = new VideoInfo()
-                    {
-                        Title = obj.Value<string>("name"),
-                        Thumb = obj.Value<string>("stream_icon"),
-                        VideoUrl = basePath + '/' + userName + '/' + password + '/' + obj.Value<string>("stream_id")
-                    };
+                    var vid = objToVideo(obj);
                     res.Add(vid);
                 }
             }
             return res;
         }
 
+        public override bool CanSearch { get { return true; } }
+
+        public override List<SearchResultItem> Search(string query, string category = null)
+        {
+            var res = new List<SearchResultItem>();
+            var search = query.Trim().ToLowerInvariant();
+
+            var data = GetWebData<JToken>(getUrl() + "get_live_streams");
+            foreach (var obj in data)
+            {
+                var vid = objToVideo(obj);
+
+                if (match(search, vid.Title.Trim().ToLowerInvariant()))
+                {
+                    var id = obj.Value<string>("category_id");
+                    if (categoryNames.TryGetValue(id, out string catName))
+                        vid.Description = catName;
+                    else
+                        search = search;
+                    res.Add(vid);
+                }
+            }
+
+            return res;
+        }
+
+        private bool match(string search, string name)
+        {
+            if (name.Contains(search))
+                return true;
+
+            var searchWords = search.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var nameWords = name.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            return searchWords.All(sw =>
+                nameWords.Any(nw => nw.StartsWith(sw)));
+        }
+
         private string getUrl()
         {
             return basePath + "/player_api.php?username=" + userName + "&password=" + password + "&action=";
+        }
+
+        private VideoInfo objToVideo(JToken obj)
+        {
+            return new VideoInfo()
+            {
+                Title = obj.Value<string>("name"),
+                Thumb = obj.Value<string>("stream_icon"),
+                VideoUrl = basePath + '/' + userName + '/' + password + '/' + obj.Value<string>("stream_id")
+            };
         }
     }
 
